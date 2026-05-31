48338680b2cc3bf47d6d1d19032dc3b4521403a4
Gui97p <134406200+Gui97p@users.noreply.github.com>
2026-05-31 16:33:50 -0300

    Change BotStatus to a placeholder
    Updated BotStatus to a placeholder message.

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #Settings for the bot.
-TOKEN = 'MTIxNjM3NzYzMDgzNjc4NTI3Mw.GIOy_P.KnTaRkz0BmZq7N8ihXiCgpH50d0Qy9ldSbuwHI'
-BotStatus = "É sexo é?"
+TOKEN = 'MTIxNjM3NzYzMDgzNjc4NTI3Mw.GIOy_P.KnTaRkz0BmZq7N8ihXiCgpH50d0Qy9ldSbuwHI' # Example token
+BotStatus = "Your own status here"
 Prefix = "."
 
 BankPerc = 5 / 100
